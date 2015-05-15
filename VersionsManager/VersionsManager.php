39d17e0bda59ceea306d6a06d5748cca8b2a2442
Claudio Giordano <claudio.giordano@autistici.org>
2015-05-15 22:27:42 +0200

    Various fix

--- VersionsManager/VersionsManager.php
+++ VersionsManager/VersionsManager.php
@@ -1,22 +1,22 @@
 <?php
 
-class VersionsManager extends MantisPlugin {
+class VersionsManagerPlugin extends MantisPlugin
+{
 
-	function register()
+    function register()
     {
-        print_r("PIPPO");
         $this->name        = 'Versions Manager';
-		$this->description = 'Advanced versions management plugin';
-		$this->page        = '';
+        $this->description = 'Advanced versions management plugin';
+        $this->page        = 'config_page';
 
-		$this->version = '0.1';
+        $this->version = '0.1';
 
         $this->requires = [
-            'MantisCore' => '1.2'
+            'MantisCore' => '1.2.0',
         ];
 
         $this->author  = 'Claudio Giordano';
-		$this->contact = 'claudio.giordano@autistici.org';
-		$this->url     = '';
-	}
+        $this->contact = 'claudio.giordano@autistici.org';
+        $this->url     = '';
+    }
 }
