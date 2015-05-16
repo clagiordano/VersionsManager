a002dc9f8e1e9da1e09c576252a701169e87ceba
Claudio Giordano <claudio.giordano@autistici.org>
2015-05-16 23:57:43 +0200

    Fix table structure and style

--- VersionsManager/pages/view.php
+++ VersionsManager/pages/view.php
@@ -1,68 +1,68 @@
 <?php
-
 access_ensure_global_level(plugin_config_get('view_versions'));
 
 html_page_top();
-
-echo '<br /><span class="pagetitle">', string_display(lang_get('projects_title')), '</span><br />';
 ?>
 
 <link rel='stylesheet' type="text/css" href="<?php echo plugin_file('style.css'); ?>" />
 
-<br />
-<table class="width100 version-table" cellspacing="1">
-    <tr class="row-category">
-        <th width="10%">
-            <?php echo lang_get('name'); ?>
-        </th>
-        <th width="10%">
-            <?php echo lang_get('version'); ?>
-        </th>
-        <th width="15%">
-            <?php echo lang_get('status'); ?>
-        </th>
-        <th width="30%">
-            <?php echo lang_get('description'); ?>
-        </th>
-        <th width="5%">
-            <?php echo lang_get('released'); ?>
-        </th>
-        <th width="10%">
-            <?php echo lang_get('scheduled_release'); ?>
-        </th>
-    </tr>
+<div class="table-container">
+    <table class="version-table">
+        <tr class="row-category">
+            <th>
+                <?php echo lang_get('name'); ?>
+            </th>
+            <th>
+                <?php echo lang_get('version'); ?>
+            </th>
+            <th>
+                <?php echo lang_get('status'); ?>
+            </th>
+            <th>
+                <?php echo lang_get('description'); ?>
+            </th>
+            <th>
+                <?php echo lang_get('released'); ?>
+            </th>
+            <th>
+                <?php echo lang_get('scheduled_release'); ?>
+            </th>
+        </tr>
 
-    <?php
+        <?php
         foreach (get_projects() as $project) {
             if ($project['released_bool'] == true) {
                 $t_class = "class=\"center released\"";
             } else {
                 $t_class = "class=\"center not-released\"";
             }
-        ?>
+            ?>
 
-        <tr <?php echo helper_alternate_class(); ?>>
-            <td>
-                <?php echo string_display($project['name']); ?>
-            </td>
-            <td class="center">
-                <?php echo string_display($project['version']); ?>
-            </td>
-            <td class="center">
-                <?php echo get_enum_element('project_status', $project['status']); ?>
-            </td>
-            <td>
-                <?php echo string_display($project['description']); ?>
-            </td>
-            <td <?php echo $t_class; ?>>
-                <?php echo string_display($project['released']); ?>
-            </td>
-            <td class="center">
-                <?php echo string_display($project['scheduled_release']); ?>
-            </td>
-        </tr>
-<?php } ?>
-</table>
+            <tr >
+                <td>
+                    <?php echo string_display($project['name']); ?>
+                </td>
+                <td class="center">
+                    <?php echo string_display($project['version']); ?>
+                </td>
+                <td class="center">
+                    <?php
+                    echo get_enum_element('project_status', $project['status']);
+                    ?>
+                </td>
+                <td>
+                    <?php echo string_display($project['description']); ?>
+                </td>
+                <td <?php echo $t_class; ?>>
+                    <?php echo string_display($project['released']); ?>
+                </td>
+                <td class="center">
+                    <?php echo string_display($project['scheduled_release']); ?>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+</div>
 
 <?php
 html_page_bottom();
